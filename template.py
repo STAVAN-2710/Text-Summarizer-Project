b0b7ec407620c1623a6f075cd7894fba27c4f890
Stavan <stavan.sanyal@gmail.com>
2023-09-03 17:15:42 -0500

    folder structure added

---
+++ template.py
@@ -0,0 +1,49 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format= '[%(asctime)s]: %(message)s')
+
+project_name = "mlProject"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/conponents/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "app.py",
+    "main.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)  #this prevents problem with the forward and backward slash
+    filedir, filename = os.path.split(filepath)
+
+#   check if folder directory is not empty, then create the folder structure and log the information.  
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Created directory: {filedir} for the file {filename}")
+
+# check if file exists or not, if size is 0, then create the file and log the information
+# if everything is okay then give file already exists
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Created file: {filepath}")
+
+    else :
+        logging.info(f"{filename} is already exists")
